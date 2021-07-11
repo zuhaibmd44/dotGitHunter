cd5f986fe973048e18ef062f8138ef86fd68dbc4
root <zuhaibmd44@gmail.com>
2021-07-11 07:10:21 -0400

    Improved the execution speed

--- dotGitHunter.sh
+++ dotGitHunter.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 #Zuhaib Mohammed - July 2021
 #This script is used to identify open .git directory for a list of subdomains
-#Command to run -> ./dotgitFinder.sh subdomains.txt
+#Command to run -> ./dotGitFinder.sh subdomains.txt
+#v2 - Improved the execution speed by sedning the process to background
+#Install the uuid-runtime package before running the script using "apt install uuid-runtime"
+
+function getResponse {
+    uuid=$(dbus-uuidgen) #unique file identifier to store output of cURL request
+    http_response=$(curl -L --insecure -s -o $uuid.txt -w "%{response_code}" $line/.git)
+    http_title=$(cat $uuid.txt | grep "<title>.*</title>" | sed -e 's/<[^>]*>//g' | awk '{$1=$1};1')
+    echo $line/.git":--:"$http_response":--:"$http_title
+    rm $uuid.txt
+}
+
 subDomains="$1"
 while IFS= read -r line
 do
-    http_response=$(curl -L --insecure -s -o response.txt -w "%{response_code}" $line/.git)
-    http_title=$(cat response.txt | grep "<title>.*</title>" | sed -e 's/<[^>]*>//g' | awk '{$1=$1};1')
-    echo $line/.git":--:"$http_response":--:"$http_title
+    getResponse "$line" & 
 done < "$subDomains"
+wait # wait for all child processes to finish
